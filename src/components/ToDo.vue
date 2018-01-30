1c5bf888fb2da8470ae991c5387070be978b0fbb
Ben Warner <benjaminnwarner@gmail.com>
2018-01-29 19:58:39 -0500

    Remove todo functionality added

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="todo">
-	<h1>To-Do List {{ message }} </h1>
+	<h1>To-Do List</h1>
 	<form v-on:submit="addToDo">
 		<input v-model="message" type="text"/>
 	</form>
@@ -19,18 +19,16 @@
 		name:"ToDo",
 		data() {
 			return {
-				message: "Hello Ben",
+				message: "",
 				todos : [],
 			}
 		},
 
 
 		created(){
-			console.log("Hey there");
 			let storedTodoJson = localStorage.getItem("todos") || "[]";
 			this.todos = JSON.parse(storedTodoJson);
 		},
-
 
 		methods : {
 
@@ -38,15 +36,20 @@
 				if(this.message){
 					let todo = { text: this.message };
 					this.todos.push(todo);
-					let todosAsJson = JSON.stringify(this.todos);
-					localStorage.setItem("todos", todosAsJson);
+					this.updateToDoStorage();
 					this.message = "";
 				}
-				return false;
 			},
 
 			removeTodo(key){
+				this.todos.splice(key, 1);
+				console.log(this.todos);
+				this.updateToDoStorage();
+			},
 
+			updateToDoStorage(){
+				let todosAsJson = JSON.stringify(this.todos);
+				localStorage.setItem("todos", todosAsJson);
 			}
 
 		}
@@ -54,6 +57,10 @@
 </script>
 
 <style>
+	#todo {
+
+	}
+	
 	ul {
 		list-style-type: none;
 		margin: 0 auto;
